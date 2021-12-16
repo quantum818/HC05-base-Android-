b0aeda7fffca50ac2d642f60a2167c532f78cc74
ironpad_ryujo <70073019+quantum818@users.noreply.github.com>
2021-12-16 18:42:38 +0900

    Create btdialog.java

---
+++ app/src/main/java/com/example/test1/btdialog.java
@@ -0,0 +1,14 @@
+package com.example.test1;
+
+import android.app.Activity;
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class btdialog extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_btdialog);
+    }
+}
