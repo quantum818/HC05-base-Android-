da0330b3f3f6d3b71849ce2743b32514f1d39e6a
ironpad_ryujo <70073019+quantum818@users.noreply.github.com>
2021-12-16 18:42:40 +0900

    Create dialog.java

---
+++ app/src/main/java/com/example/test1/dialog.java
@@ -0,0 +1,12 @@
+package com.example.test1;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import androidx.fragment.app.DialogFragment;
+
+public class dialog
+{
+
+}
