e7debcea527a25f3498970e87b90740a98bcf71e
ironpad_ryujo <70073019+quantum818@users.noreply.github.com>
2022-01-22 02:28:17 +0900

    Update MyApplication.java

--- app/src/main/java/com/example/test1/MyApplication.java
+++ app/src/main/java/com/example/test1/MyApplication.java
@@ -1,8 +1,11 @@
 package com.example.test1;
 
 import android.app.Application;
+import android.app.Notification;
+import android.os.Message;
 
 public class MyApplication extends Application
 {
     static String Macnow="";
+    static Message sendcomp=new Message();
 }
