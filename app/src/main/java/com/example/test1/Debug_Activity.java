b5b8d64e4ce09e30b5621fcf770dacc291e347f7
ironpad_ryujo <70073019+quantum818@users.noreply.github.com>
2022-01-22 02:28:14 +0900

    Update Debug_Activity.java

--- app/src/main/java/com/example/test1/Debug_Activity.java
+++ app/src/main/java/com/example/test1/Debug_Activity.java
@@ -5,6 +5,8 @@ import android.app.Dialog;
 import android.bluetooth.BluetoothAdapter;
 import android.bluetooth.BluetoothDevice;
 import android.os.Build;
+import android.os.Handler;
+import android.os.Message;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
@@ -17,6 +19,8 @@ import java.io.UnsupportedEncodingException;
 import java.nio.charset.StandardCharsets;
 import java.util.Calendar;
 
+import static com.example.test1.MyApplication.sendcomp;
+
 public class Debug_Activity extends AppCompatActivity {
     public void printLog(String str) {
         Log.e("print", str);
@@ -26,41 +30,65 @@ public class Debug_Activity extends AppCompatActivity {
     private ConnectedThread nana7mi;
     private static MyBluetooth.ConnectThread mConnectThread;
     private String getinfo="";
-    Button send;
+    Button send,empty;
+/*    Handler handler=new Handler(){
+        @Override
+        public void handleMessage(Message msg) {
+            super.handleMessage(msg);
+            if(msg.what==773){
+                send.setClickable(false);
+                send.setBackgroundColor(0xffff0000);
+            }
+            else if(msg.what==1551){
+                send.setClickable(true);
+            }
+        }
+    };*/
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_debug_);
+        EditText sendmessage,getmessages;
+        sendmessage=findViewById(R.id.sendinfo);
+        getmessages=findViewById(R.id.getinfo);
         send=(Button) findViewById(R.id.send);
+        empty=(Button) findViewById(R.id.empty);
+        send.setEnabled(false);
+        send.setText("寸止!");
+        String temp=allres.Macnow;
+        BluetoothAdapter mybluetooth=BluetoothAdapter.getDefaultAdapter();
+        mybluetooth.enable();//打开蓝牙
+        bluetools.mBtAdapter=mybluetooth;
+        BluetoothDevice bluetoothDevice = null;
+        try {
+            bluetoothDevice = mybluetooth.getRemoteDevice(temp);
+            bluetools.connect(bluetoothDevice);
+            send.setEnabled(true);
+            send.setText("发射!");
+        } catch (Exception e) {
+            printLog(e.toString());
+            @SuppressLint("WrongConstant") Toast warningempty = Toast.makeText(Debug_Activity.this, "MAC格式错误或为空", 240);
+            warningempty.show();
+            Dialog dialog = new Dialog(Debug_Activity.this);
+            View warning1 = getLayoutInflater().inflate(R.layout.warning1,null);
+            dialog.setContentView(warning1);
+            dialog.show();
+        }
+        empty.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                getmessages.setText("");
+            }
+        });
         send.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                EditText sendmessage,getmessages;
-                sendmessage=findViewById(R.id.sendinfo);
-                getmessages=findViewById(R.id.getinfo);
-                String temp=allres.Macnow;
                 String sendmessagestr=(sendmessage.getText()).toString();
                 if((sendmessage.getText()).length()==0){
                     @SuppressLint("WrongConstant") Toast toast = Toast.makeText(Debug_Activity.this, "输入不能为空", 1000);
                     toast.show();
                 }
                 else{
-                    BluetoothAdapter mybluetooth=BluetoothAdapter.getDefaultAdapter();
-                    mybluetooth.enable();//打开蓝牙
-                    bluetools.mBtAdapter=mybluetooth;
-                    BluetoothDevice bluetoothDevice = null;
-                    try {
-                        bluetoothDevice = mybluetooth.getRemoteDevice(temp);
-                        bluetools.connect(bluetoothDevice);
-                    } catch (Exception e) {
-                        printLog(e.toString());
-                        @SuppressLint("WrongConstant") Toast warningempty = Toast.makeText(Debug_Activity.this, "MAC格式错误或为空", 240);
-                        warningempty.show();
-                        Dialog dialog = new Dialog(Debug_Activity.this);
-                        View warning1 = getLayoutInflater().inflate(R.layout.warning1,null);
-                        dialog.setContentView(warning1);
-                        dialog.show();
-                    }
                     if(bluetools.nanami==null){
                         printLog("nanami is empty");
                     }
@@ -72,6 +100,8 @@ public class Debug_Activity extends AppCompatActivity {
                             e.printStackTrace();
                         }
                         bluetools.nanami.write(sendbytes);
+                        //handler.sendMessage(sendcomp);
+                        printLog("handle: "+String.valueOf(sendcomp.what));
                         Calendar caltools=Calendar.getInstance();
                         String hour= String.valueOf(caltools.get(Calendar.HOUR_OF_DAY));
                         String min= String.valueOf(caltools.get(Calendar.MINUTE));
