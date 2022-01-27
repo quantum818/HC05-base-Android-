7fee4525a587252b9b7e676004b4a4641b560132
ironpad_ryujo <70073019+quantum818@users.noreply.github.com>
2022-01-27 16:42:31 +0900

    Update MyApplication.java

--- app/src/main/java/com/example/test1/MyApplication.java
+++ app/src/main/java/com/example/test1/MyApplication.java
@@ -2,6 +2,7 @@ package com.example.test1;
 
 import android.app.Application;
 import android.app.Notification;
+import android.content.Context;
 import android.os.Message;
 
 public class MyApplication extends Application
